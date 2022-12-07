7dd44fb2e83bd2abe9bf9b1cb45283869a843c41
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-07 11:26:39 +0530

    functions

---
+++ scripts/function.js
@@ -0,0 +1,80 @@
+//functions
+
+function printMessage(message){
+    console.log(message);
+}
+
+//sum
+function sum(x,y){
+    let sumofNum= x+y;
+    return sumofNum;
+}
+
+let result = sum(5,3);
+
+//substraction
+function substraction(x,y){
+    let substractionofNum = x-y;
+    return substractionofNum;
+}
+
+let result1 = substraction(9,8);
+
+//multiplication
+function mul(x,y){
+    let multiplicationofNum = x*y;
+    return multiplicationofNum;
+}
+
+let result2 = mul(5,5);
+
+//division
+function division(x,y){
+    let division0fNum = x/y;
+    return division0fNum;
+}
+
+let result3 = division(8,4);
+
+//modulo
+function modulo(x,y){
+    let moduloofNum = x%y;
+    return moduloofNum;
+}
+
+let result4 = modulo(10,6);
+//printMessages
+printMessage(result);
+printMessage(result1);
+printMessage(result2);
+printMessage(result3);
+printMessage(result4);
+
+
+function greet(user){
+    return `hello ${user}!!`
+}
+
+ let user = "bhargav mandapaka";
+ let string = greet(user);
+ console.log(string);
+
+ function greet1(firstName,Lastname){
+    console.log(`Welcome ${firstName} ${Lastname}`);
+ }
+
+ greet1("bhargav","Mandapaka");
+
+
+// function printMessage(message){
+//     console.log(message);
+// }
+
+ function add(x,y){
+    return  x+y;
+ }
+
+//  let sum=add1;
+ let result5 = add(8,20);
+//  console.log(result);
+printMessage(result5);
