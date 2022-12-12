84c4bdf563191d8692d2986b427d0f9b8dd06d37
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-12 21:17:07 +0530

    work done

---
+++ scripts/box.js
@@ -0,0 +1,32 @@
+document.getElementById('demo').style.backgroundColor='red';
+document.getElementById('demo').innerText= 'im red '
+document.getElementById('demo').style.height= '200px';
+document.getElementById('demo').style.width= '200px';
+
+document.getElementById('demo1').style.backgroundColor='orange';
+document.getElementById('demo1').innerText= 'im orange'
+document.getElementById('demo1').style.height= '200px';
+document.getElementById('demo1').style.width= '200px';
+
+
+document.getElementById('demo2').style.backgroundColor='aqua';
+document.getElementById('demo2').innerText= 'im aqua'
+document.getElementById('demo2').style.height= '200px';
+document.getElementById('demo2').style.width= '200px';
+
+
+document.getElementById('demo3').style.backgroundColor='green';
+document.getElementById('demo3').innerText= 'im green'
+document.getElementById('demo3').style.height= '200px';
+document.getElementById('demo3').style.width= '200px';
+
+
+document.getElementById('demo4').style.backgroundColor='blue';
+document.getElementById('demo4').innerText= 'im blue'
+document.getElementById('demo4').style.height= '200px';
+document.getElementById('demo4').style.width= '200px';
+
+document.getElementById('demo5').style.backgroundColor='violet';
+document.getElementById('demo5').innerText= 'im violet'
+document.getElementById('demo5').style.height= '200px';
+document.getElementById('demo5').style.width= '200px';
