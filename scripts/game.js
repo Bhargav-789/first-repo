cd0e6e36aa80ebda8855d3f85f5f400bedbd422f
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-13 16:39:15 +0530

    cmplt

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+// function getRandaomNum(max){
+//      console.log(Math.floor(Math.random()*max));
+//     // return  random;
+// }
+//  console.log(getRandaomNum(100));
+
+function ranDom(){
+    document.getElementById('random').innerHTML= Math.floor(Math.random()*100);
+}
+ 
