d4d68e9a5323b809e893b279cd0fe1338e1c8c27
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-13 22:55:39 +0530

    Today work

---
+++ scripts/game.js
@@ -0,0 +1,11 @@
+function random1(){
+    document.getElementById('random1').innerHTML = Math.floor(Math.random()*100);
+}
+
+
+ function random2(){
+    document.getElementById('random2').innerHTML = Math.floor(Math.random()*100);
+ }
+
+
+
