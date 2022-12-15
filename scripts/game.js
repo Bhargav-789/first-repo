0bf0a93ddc62c5ee87b04ccc7fab9e72ea143a3e
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-15 16:01:56 +0530

    work

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,28 @@
-function random1(){
-    document.getElementById('random1').innerHTML = Math.floor(Math.random()*100);
+let button1 = document.querySelector('#leftButton');
+let button2 = document.querySelector('#rightButton');
+let p1value = document.querySelector('#random1');
+let p2value = document.querySelector('#random2');
+
+function generateRandom() {
+    let randomNumber = Math.floor(Math.random()*100); 
+    return randomNumber;
 }
 
+button1.addEventListener('click', () =>{
+    p1value.innerText = generateRandom();
 
- function random2(){
-    document.getElementById('random2').innerHTML = Math.floor(Math.random()*100);
- }
+});
 
+button2.addEventListener('click', () =>{
+    p2value.innerText = generateRandom();
+});
 
+let p1Arr = [];
+let p2Arr = [];
 
+function sum(){
+    let sum = 0;
+    for(let i=0; i<=5; i++){
+        sum+= 
+    }
+}
