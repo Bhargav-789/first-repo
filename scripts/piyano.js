b3381977caec3bc65abb29ad1d6dee88319ec464
Bhargav <bhargavmandapaka789@gmail.com>
2022-12-23 12:38:25 +0530

    piyano

--- scripts/piyano.js
+++ scripts/piyano.js
@@ -1,1 +1,66 @@
-let 
+// Tune 1
+let audioC = new Audio("./assests/A.mp3");
+const playTune1 = () =>{
+    audioC.play();
+};
+
+//Tune2
+let audioCash = new Audio("./assests/Ab.mp3");
+const playTune2= () =>{
+    audioCash.play();
+};
+
+//Tune3
+let audioD = new Audio("./assests/D.mp3");
+
+const playTune3 = () =>{
+    audioD.play();
+};
+
+//Tune4
+let audioDash = new Audio("./assests/Db.mp3");
+const playTune4 = () =>{
+    audioDash.play();
+};
+
+//Tune5 
+let audioE = new Audio("./assests/E.mp3");
+const playTune5=() =>{
+ audioE.play();
+};
+
+//Tune6
+let audioFash = new Audio("./assests/F.mp3");
+const playTune6=()=>{
+    audioFash.play();
+};
+
+//Tune7
+let audioF = new Audio("./assests/F.mp3");
+const playTune7=()=>{
+    audioFash.play();
+};
+
+//Tune8
+let audioG = new Audio("./assests/G.mp3");
+const playTune8=()=>{
+    audioG.play();
+};
+
+//Tune9
+let audioGash = new Audio("./assests/Gb.mp3");
+const playTune9=()=>{
+    audioGash.play();
+};
+
+//Tune10
+let audioA = new Audio("./assests/C.mp3");
+const playTune10=()=>{
+    audioA.play();
+};
+
+//Tune11
+let audioAash = new Audio("./assests/Cb.mp3");
+const playTune11=() =>{
+    audioAash.play();
+};
